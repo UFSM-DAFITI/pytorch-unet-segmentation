6f16e8f5864ce7b03b5a1a5a8311254030162756
Marcos Hideo Maruo <maruo@ieee.org>
2022-05-09 11:12:56 -0300

    QoL: Added progress bars to downloadweights.py for slow connections

--- downloadweights.py
+++ downloadweights.py
@@ -1,6 +1,9 @@
 import sys
 if __name__ == "__main__":
     import urllib.request
+    import downloadprogressbar
 
-    urllib.request.urlretrieve("https://docs.google.com/uc?export=download&id=1sC_puW3pc6P75KTi2hJxgjgUiZo3zl1Q&confirm=t", "unet.pt")
-    urllib.request.urlretrieve("https://docs.google.com/uc?export=download&id=1ucX0HD8JV10GFol7cOXHe5W7GAh176Zo&confirm=t", "metahuman.png")
+    print (f'Downloading neural network weights')
+    urllib.request.urlretrieve("https://docs.google.com/uc?export=download&id=1sC_puW3pc6P75KTi2hJxgjgUiZo3zl1Q&confirm=t", "unet.pt", downloadprogressbar.DownloadProgressBar())
+    print (f'Downloading avatar')
+    urllib.request.urlretrieve("https://docs.google.com/uc?export=download&id=1ucX0HD8JV10GFol7cOXHe5W7GAh176Zo&confirm=t", "metahuman.png", downloadprogressbar.DownloadProgressBar())
